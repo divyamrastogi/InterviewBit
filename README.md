98c49b1011fb9f9e5f21ca5cca3c04fc056f90b9
Divyam Rastogi <divyamr@flock.com>
2019-05-06 22:07:54 +0530

    Cover Points solution added

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ This repository contains solutions to problems I've come across and solved on [I
 
 ### Problems and solutions
 - [Infinite Grid](https://www.interviewbit.com/problems/min-steps-in-infinite-grid/) - [coverPoints.js](https://github.com/divyamrastogi/InterviewBit/blob/master/array/coverPoints.js)
-- [Max Sum Contiguous Array](https://www.interviewbit.com/problems/max-sum-contiguous-subarray/) - [arrayManipulation.js](https://github.com/divyamrastogi/InterviewBit/blob/master/array/maxSumContiguous.js)
+- [Max Sum Contiguous Array](https://www.interviewbit.com/problems/max-sum-contiguous-subarray/) - [maxSumContiguous.js](https://github.com/divyamrastogi/InterviewBit/blob/master/array/maxSumContiguous.js)
+- [Min steps in Infinite Grid](https://www.interviewbit.com/problems/min-steps-in-infinite-grid/) - [coverPoints.js](https://github.com/divyamrastogi/InterviewBit/blob/master/array/coverPoints.js)
--- array/coverPoints.js
+++ array/coverPoints.js
@@ -1,17 +1,17 @@
 var s = {
   coverPoints: function(A, B) {
-      var totalSteps = 0;
-      var currX = A[0];
-      var currY = B[0];
-      for (var i = 1; i < A.length; i++) {
-        var common = Math.min(Math.abs(currX - A[i]), Math.abs(currY - B[i]));
-        var steps = Math.abs(currX - A[i]) + Math.abs(currY - B[i]);
-        totalSteps = totalSteps + steps - common;
-        currX = A[i];
-        currY = B[i];
-      }
-      console.log(totalSteps);
+    var totalSteps = 0;
+    var currX = A[0];
+    var currY = B[0];
+    for (var i = 1; i < A.length; i++) {
+      var common = Math.min(Math.abs(currX - A[i]), Math.abs(currY - B[i]));
+      var steps = Math.abs(currX - A[i]) + Math.abs(currY - B[i]);
+      totalSteps = totalSteps + steps - common;
+      currX = A[i];
+      currY = B[i];
+    }
+    return totalSteps;
   }
 };
 
-s.coverPoints([1, 2, 6], [1, 2, 6])
+s.coverPoints([1, 2, 6], [1, 2, 6]);
